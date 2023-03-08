64c24acc61b61b981aef9b0ebe896d5721bdacf8
Ekaterina <santeh.lg@gmail.com>
2023-03-08 23:45:21 +0300

    done Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт является ли число чётным или нет
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+  Console.WriteLine($"{n} - чётное число");
+if (n % 2 == 1)
+  Console.WriteLine($"{n} - нечётное число");
+
