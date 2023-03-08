ea051110f9b0f83ceb7596e40dfe9b35b1f9085b
Ekaterina <santeh.lg@gmail.com>
2023-03-08 22:15:51 +0300

    done Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿//На вход принимает два числа и выдает какое больше, какое меньше.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+Console.WriteLine($"{a} максимальное число, {b} минимальное число");
+else if(a < b)
+Console.WriteLine($"{b} максимальное число, {a} минимальное число");
+else
+Console.WriteLine("Вы ввели равные числа");
