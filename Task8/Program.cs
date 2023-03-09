071e877dd6dc704631671774ceafadb8e8fb08f8
Ekaterina <santeh.lg@gmail.com>
2023-03-09 18:53:49 +0300

    done Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на ввод число N, а на выводе показывает все чётные числа от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+for(int i = 1; i < n; i++) {
+if(i%2==0)
+Console.Write($"{i} ");
+}
+
+ 
