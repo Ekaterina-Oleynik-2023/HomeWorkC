35d5ea08c0b75783380ea4befd719e9e5d253786
Ekaterina <santeh.lg@gmail.com>
2023-03-11 18:04:01 +0300

    Семинар_2 done Nask10

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите любое трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 100 || n > 1000)
+{
+    Console.Write("Вы ошиблись!\nВведите другое число(от 100 до 999): ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int n1 = (n / 10);
+int n2 = (n1 % 10);
+Console.WriteLine(n2);
+
