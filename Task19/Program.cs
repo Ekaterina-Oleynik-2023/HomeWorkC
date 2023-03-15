fc4787f10979bb4caebeed6d556227e2b264639d
Ekaterina <santeh.lg@gmail.com>
2023-03-15 11:25:05 +0300

    Семинар 3 Задача 21

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число");
+x = 0;
+y =0;
+d =0;
+c = 0;
+string s = Console.ReadLine()!;
+x = int.Parse (s[0].ToString());
+y = int.Parse (s[1].ToString());
+d = int.Parse (s[3].ToString());
+c = int.Parse (s[4].ToString());
+if (x == c && y == d )
+Console.WriteLine("да");
+else 
+Console.WriteLine("нет");
--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,10 +1,11 @@
 ﻿// Программа принимает на ввод число N, а на выводе показывает все чётные числа от 1 до N
 Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine()!);
-for(int i = 1; i < n; i++) {
-if(i%2==0)
-Console.Write($"{i} ");
-}
+Console.WriteLine($"Ввест чмсло (n1)");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ввест чмсло (n2)");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 * n1 ==n2 || n2 * n2 ==n1)
+Console.WriteLine("верно");
+else 
+Console.WriteLine("не верно");
 
- 
